d3b0957437f993e9619e20f2956b081ada46edba
toanmx0101 <wth8647@gmail.com>
2018-10-01 23:06:37 +0700

    Install Mongodb

--- project/frontend/urls.py
+++ project/frontend/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('aaa', views.index ),
-    path('show', views.show )
+    path('', views.index ),
+    path('show', views.show)
 ]
